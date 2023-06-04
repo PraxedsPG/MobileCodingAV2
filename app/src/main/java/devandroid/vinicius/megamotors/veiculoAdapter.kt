398be2b3c19a160c811cf1de6351e732a36ae7de
Praxedes <112719512+PraxedsPG@users.noreply.github.com>
2023-06-04 12:35:35 -0300

    Pojeto Atualizado

--- app/src/main/java/devandroid/vinicius/megamotors/veiculoAdapter.kt
+++ app/src/main/java/devandroid/vinicius/megamotors/veiculoAdapter.kt
@@ -1,32 +1,44 @@
 package devandroid.vinicius.megamotors
 
-import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import devandroid.vinicius.megamotors.databinding.VeiculoItemBinding
-
-class veiculoAdapter(private val context: Context, private val listaDeVeiculo: MutableList<dbMegaVeiculos>):
-    RecyclerView.Adapter<veiculoAdapter.VeiculoViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VeiculoViewHolder {
-        val itemLista = VeiculoItemBinding.inflate(LayoutInflater.from(context), parent, false)
-        return VeiculoViewHolder(itemLista)
+class veiculoAdapter(private val veicList: ArrayList<dbMegaVeiculos>) :
+    RecyclerView.Adapter<veiculoAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val veicmarca : TextView = itemView.findViewById(R.id.txtMarca)
+        val veicano : TextView = itemView.findViewById(R.id.txtAno)
+        val veicmodelo : TextView = itemView.findViewById(R.id.txtModelo)
     }
 
-    override fun onBindViewHolder(holder: VeiculoViewHolder, position: Int) {
-        holder.txtMarca.text = listaDeVeiculo [position].userMark
-        holder.txtModelo.text = listaDeVeiculo [position].userModel
-        holder.txtAno.text = listaDeVeiculo [position].userYear
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.veiculo_item, parent, false)
+        return ViewHolder(itemView)
     }
 
-    override fun getItemCount() = listaDeVeiculo.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentMarca = veicList[position]
+        holder.veicmarca.text = currentMarca.userMark
 
-    inner class VeiculoViewHolder(binding: VeiculoItemBinding): RecyclerView.ViewHolder(binding.root) {
-        val txtMarca = binding.txtMarca
-        val txtModelo = binding.txtModelo
-        val txtAno = binding.txtAno
+        val currentAno = veicList[position]
+        holder.veicano.text = currentAno.userYear
+
+        val currentModelo = veicList[position]
+        holder.veicmodelo.text = currentModelo.userModel
     }
+
+    override fun getItemCount(): Int {
+        return veicList.size
+    }
+
+
 
 }
+
+
--- app/src/main/java/devandroid/vinicius/megamotors/veiculos.kt
+++ app/src/main/java/devandroid/vinicius/megamotors/veiculos.kt
@@ -2,25 +2,63 @@ package devandroid.vinicius.megamotors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
-import devandroid.vinicius.megamotors.databinding.ActivityVeiculosBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class veiculos : AppCompatActivity() {
 
-    private lateinit var binding: ActivityVeiculosBinding
-    private lateinit var adapterVeiculo: veiculoAdapter
-    private val listaVeiculos: MutableList<dbMegaVeiculos> = mutableListOf()
+    private lateinit var carRecyclerView:RecyclerView
+    private lateinit var tvLoadingData: TextView
+    private lateinit var veicList: ArrayList<dbMegaVeiculos>
+    private lateinit var dbRef:DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityVeiculosBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_veiculos)
 
-        val recyclerViewVeiculos = binding.recyclerViewVeiculos
-        recyclerViewVeiculos.layoutManager = LinearLayoutManager(this)
-        recyclerViewVeiculos.setHasFixedSize(true)
-        adapterVeiculo = veiculoAdapter(this,listaVeiculos)
-        recyclerViewVeiculos.adapter = adapterVeiculo
+        carRecyclerView = findViewById(R.id.listVeiculos)
+        carRecyclerView.layoutManager = LinearLayoutManager(this)
+        carRecyclerView.setHasFixedSize(true)
+        tvLoadingData = findViewById(R.id.tvLoadingData)
+
+        veicList = arrayListOf<dbMegaVeiculos>()
+
+        getVeiculosData()
+    }
+
+    private fun getVeiculosData() {
+        carRecyclerView.visibility = View.GONE
+        tvLoadingData.visibility = View.VISIBLE
+
+    dbRef = FirebaseDatabase.getInstance().getReference("Carro")
+
+    dbRef.addValueEventListener(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            veicList.clear()
+            if(snapshot.exists()) {
+                for(empSnap in snapshot.children) {
+                    val veicData =empSnap.getValue(dbMegaVeiculos::class.java)
+                    veicList.add(veicData!!)
+                }
+                val vAdapter = veiculoAdapter(veicList)
+                carRecyclerView.adapter = vAdapter
+
+                carRecyclerView.visibility = View.VISIBLE
+                tvLoadingData.visibility = View.GONE
+            }
+        }
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+    })
     }
 }
